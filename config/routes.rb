1abac8749ab396f3798321605b8048527fa4bc26
Kevin <kevinsevinche@gmail.com>
2021-12-21 01:12:55 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'properties/index'
+  get 'properties/show'
+  get '/properties', to: 'properties#index', as: 'properties_list'
+  get '/properties/:property_id', to: 'properties#show', as: 'property_details'
+  post '/properties/:property_id', to: 'properties#new_lead', as: 'new_lead'
+  root to: 'properties#index'
 end
